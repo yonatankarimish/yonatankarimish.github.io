0440f56a7a7be6ee6b8e01d70891f9d0928cdb37
Yonatan Alon <yonatankarimish@gmail.com>
2016-12-09 18:55:25 +0200

    changed to ui-Router

--- resources/controllers/instagular.js
+++ resources/controllers/instagular.js
@@ -1,9 +1,26 @@
-angular.module('instagular', ["ngRoute"]);
+angular.module('instagular', ["ui.router"]);
 
 angular.module('instagular').constant("clientID", "c46ca01523264d949df297d6bd4b06f9");
 
 angular.module('instagular')
-    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    .config(function($stateProvider) {
+      $stateProvider
+          .state({
+            name: 'hello',
+            url: '/',
+            templateUrl : "/resources/html/auth.html",
+            controller: 'authCtrl',
+            controllerAs: 'auth'
+          })
+          .state({
+            name: 'photos',
+            url: '/photos',
+            templateUrl : "/resources/html/photos.html",
+            controller: 'photoCtrl',
+            controllerAs: 'photos'
+          });
+    });
+    /*.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
         .when("/photos/:token", {
             templateUrl : "/resources/html/photos.html",
@@ -19,4 +36,4 @@ angular.module('instagular')
             templateUrl : "/resources/html/auth.html"
         })
     $locationProvider.html5Mode(true);
-}]);
+}]);*/
