b2f2922c2d23efd3034b64177bfada4877a56080
Yonatan Alon <yonatankarimish@gmail.com>
2016-12-13 21:04:40 +0200

    added spinner load graphic; cache now invalidates after initial load;

--- sources/photo-album/albumCtrl.js
+++ sources/photo-album/albumCtrl.js
@@ -3,33 +3,34 @@ angular.module('instagular').controller('albumCtrl', ['$scope', '$http', '$cooki
 
     album.data = [];
     album.cache = localStorage.getItem('album_cache') != null ? JSON.parse(localStorage.getItem('album_cache')) : [];
+    album.loading = true;
 
     album.findAll = function(sortCache){
+        album.loading = true;
         if(album.cacheContainsTag("")){
             album.data = album.retrieveDataForTag("");
             if(sortCache)
                 album.insertIntoCache("", album.data); //this is done to sort the cache according to search order
         }
-        else{
-            InstagramAPI.fetchPhotos(album.token, function(responseData){
-                 album.insertIntoCache("", responseData);
-                 album.data = responseData;
-            });
-        }
+        InstagramAPI.fetchPhotos(album.token, function(responseData){
+             album.insertIntoCache("", responseData);
+             album.data = responseData;
+             album.loading = false;
+        });
     }
 
     album.findByTag = function(searchTag, sortCache){
+        album.loading = true;
         if(album.cacheContainsTag(searchTag)){
             album.data = album.retrieveDataForTag(searchTag);
             if(sortCache)
                 album.insertIntoCache(searchTag, album.data); //this is done to sort the cache according to search order
         }
-        else{
-            InstagramAPI.fetchPhotosByTag(album.token, searchTag, function(responseData){
+        InstagramAPI.fetchPhotosByTag(album.token, searchTag, function(responseData){
              album.insertIntoCache(searchTag, responseData);
              album.data = responseData;
-            });
-        }
+             album.loading = false;
+        });
     }
 
     album.cacheContainsTag = function(searchTag){
